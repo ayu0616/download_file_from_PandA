6b390378e2d836d600da5c742bc202f218529a90
ayu0616 <ayumuogawa0616@gmail.com>
2022-11-03 17:47:58 +0900

    複数ファイルを連続でダウンロードできるように変更

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import os
+import urllib.parse
+
 from panda_session import PandaSession
 from settings import DOWNLOAD_DIR, PASSWORD, USER_NAME
-import urllib.parse
 
 if not DOWNLOAD_DIR:
     raise Exception(".env の DOWNLOAD_DIR が指定されていません")
 
-url = input("please enter a url => ").strip()
-if not url.startswith("https://panda.ecs.kyoto-u.ac.jp/"):
-    raise Exception("please enter a url of PandA")
-
 if not USER_NAME or not PASSWORD:
     raise Exception(".env の USER_NAME または PASSWORD が指定されていません")
 
+print("start login to PandA")
 session = PandaSession(USER_NAME, PASSWORD)
 session.login()
-res = session.get(url)
+print("finish login to PandA")
 
-file_name = urllib.parse.unquote(url.split("/")[-1])
-file_path = os.path.join(DOWNLOAD_DIR, file_name)
-with open(file_path, "wb") as f:
-    f.write(res.content)
+
+def download(url: str):
+    if not url.startswith("https://panda.ecs.kyoto-u.ac.jp/"):
+        print("please enter a url of PandA")
+        return
+
+    print("start download")
+    res = session.get(url)
+
+    file_name = urllib.parse.unquote(url.split("/")[-1])
+    file_path = os.path.join(DOWNLOAD_DIR, file_name)
+    with open(file_path, "wb") as f:
+        f.write(res.content)
+    print(f"finished download : {file_path}")
+
+
+def input_url():
+    url = input("please enter a url => ").strip()
+    return url
+
+
+url = input_url()
+while url:
+    download(url)
+    url = input_url()
